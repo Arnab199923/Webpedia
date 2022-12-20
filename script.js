27f897a2402b76d35d398abaee664a5e8cfb0c8e
Arnab199923 <96194042+Arnab199923@users.noreply.github.com>
2022-12-20 06:02:18 +0530

    full code change

---
+++ script.js
@@ -0,0 +1,25 @@
+// let persom={
+//     name:'arnab',
+//     age:22,
+//     qualification:'frontend developer'
+// }
+// let heading = document.querySelector('h1')
+// let get = persom.name
+// let test=()=>{
+//     heading.innerText=get
+//     }
+    
+    
+    
+    function test(){
+        let get = document.getElementById('inpt')
+        let heading = document.querySelector('h1').innerText=get.value
+        document.getElementById('visible').style.display='none';
+        document.getElementById('reload').style.visibility='visible'
+    
+}
+
+let cls=()=>{
+    console.log('button clicked');
+    location. reload()
+}
